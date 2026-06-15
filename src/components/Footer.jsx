6017e90c0e18a759af4c26c74836851dd9f83669
AnuCS <anannyak007@gmail.com>
2026-06-15 09:58:29 +0530

    add privacy and termscondition page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <footer className="bg-dark text-white py-5">
@@ -17,11 +19,62 @@ function Footer() {
     <h4>Quick Links</h4>
 
     <ul className="list-unstyled">
-      <li>Home</li>
-      <li>About</li>
-      <li>Services</li>
-      <li>Contact</li>
-    </ul>
+
+  <li>
+    <Link
+      to="/"
+      className="text-white text-decoration-none"
+    >
+      Home
+    </Link>
+  </li>
+
+  <li>
+    <Link
+      to="/about"
+      className="text-white text-decoration-none"
+    >
+      About
+    </Link>
+  </li>
+
+  <li>
+    <Link
+      to="/services"
+      className="text-white text-decoration-none"
+    >
+      Services
+    </Link>
+  </li>
+
+  <li>
+    <Link
+      to="/contact"
+      className="text-white text-decoration-none"
+    >
+      Contact
+    </Link>
+  </li>
+
+  <li>
+    <Link
+      to="/privacy-policy"
+      className="text-white text-decoration-none"
+    >
+      Privacy Policy
+    </Link>
+  </li>
+
+  <li>
+    <Link
+      to="/terms-conditions"
+      className="text-white text-decoration-none"
+    >
+      Terms & Conditions
+    </Link>
+  </li>
+
+</ul>
   </div>
 
   <div className="col-md-4">
